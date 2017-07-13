312a3861f06c1be29c143a4a721ad8877b2d0dae
Asem Alaa <asem_alla@msn.com>
2017-07-14 01:31:51 +0200

    restructure
    Former-commit-id: 20a5e7df6c5a2e12929992568dad94d4dc11b519

---
+++ src/test/tests.cpp
@@ -0,0 +1,5 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main()
+
+#include "tests.h"
+#include "catch/catch.hpp"
+
---
+++ src/test/tests.h
@@ -0,0 +1,36 @@
+#ifndef TESTS_H
+#define TESTS_H
+
+#define DATA_DIRECTORY "E:/repo/wevote-service/data"
+
+
+namespace test_utils
+{
+    auto getFileLines( const std::string &filePath )
+    {
+        std::ifstream f( filePath );
+        std::vector< std::string > lines;
+        std::string line;
+        if( f )
+            while( std::getline( f , line ))
+                lines.push_back( line );
+        else std::cout << "Failed to open file:" << filePath ;
+        return lines;
+    }
+
+
+    template< typename Container1 , typename Container2 ,
+              typename Elem = typename Container1::value_type >
+    bool setBasedEquality( const Container1 &a , const Container2 &b ,
+                           bool checkSize = true )
+    {
+        if( checkSize && a.size() != b.size())
+            return false;
+
+        std::set< Elem > setA( std::begin( a ) , std::end( a ));
+        std::set< Elem > setB( std::begin( b ) , std::end( b ));
+        return setA == setB;
+    }
+
+}
+#endif // TESTS_H
