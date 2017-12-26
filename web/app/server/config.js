541f3bfd9e808f119ad8aaaaccef932791196d54
Asem Alaa <asem_alla@msn.com>
2017-12-26 17:42:53 +0200

    resolve mongo address

--- web/app/server/config.js
+++ web/app/server/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://127.0.0.1:27017/wevote',
+    'mongoUrl': 'mongodb://mongo/wevote',
     'host': '0.0.0.0',
     'url': 'http://127.0.0.1',
     'port': 3000,
